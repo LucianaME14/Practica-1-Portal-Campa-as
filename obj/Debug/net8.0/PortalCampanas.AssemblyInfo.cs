6beffc53e3d2746109c468472a5df993b1a06a01
LucianaME14 <lucianamorenoesp14@gmail.com>
2026-03-28 13:16:46 -0500

    agregar detalle de campaña

--- obj/Debug/net8.0/PortalCampanas.AssemblyInfo.cs
+++ obj/Debug/net8.0/PortalCampanas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortalCampanas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00e91aaa8a712e15d41476bdbc18eba01da293a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fca5e053b0498207fa30786d0ae85a54f43ca17c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortalCampanas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortalCampanas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
